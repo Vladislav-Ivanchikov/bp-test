40da8332777a5b8441ae8f06536cc882df00bde1
Vladislav <sexxrab@gmail.com>
2022-08-26 19:18:37 +0300

    add adaptive

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 /// Set language
+
 const languages = ['en', 'ru', 'es', 'fr', 'nl', 'zh', 'ja']
 let lang = navigator ? navigator.language.substring(0, 2).toLowerCase() : "en"
 
 function searchLang(langArr) {
     let query = window.location.search
     let option = query.substring(6)
-    if (query.startsWith('?lang=') && langArr.some(el => el === option)){
+    if (query.startsWith('?lang=') && langArr.some(el => el === option)) {
         lang = option
-    }
-    else {
+    } else {
         return null
     }
 }
@@ -20,14 +20,18 @@ getContentByLang().then()
 const restore = document.querySelectorAll('.header-item')[1].children[0]
 
 // Title
+const titleContainer = document.querySelector('.title')
 const title = document.querySelector('h1')
 
 // Content
+const contentContainer = document.querySelector('.content')
 const contentItem1 = document.querySelectorAll('.text-item')[0].children[0]
 const contentItem2 = document.querySelectorAll('.text-item')[1].children[0]
 const contentItem3 = document.querySelectorAll('.text-item')[2].children[0]
 
 // Sub
+const subContainer = document.querySelector('.sub')
+const subType = document.querySelectorAll('.sub-type')
 const monthCard = document.querySelectorAll('.card')[0]
 const annualCard = document.querySelectorAll('.card')[1]
 const monthSub = document.querySelectorAll('.sub-type')[0]
@@ -38,6 +42,7 @@ const monthLine = document.querySelectorAll('.line')[0]
 const annualLine = document.querySelectorAll('.line')[1]
 const perMonth = document.querySelectorAll('.per-month')[0]
 const perAnnual = document.querySelectorAll('.per-month')[1]
+const line = document.querySelectorAll('.line')
 const sale = document.querySelector('.sale')
 
 // Button
@@ -49,9 +54,11 @@ const termsOfUse = document.querySelector('.links').children[0]
 const privacyPolicy = document.querySelector('.links').children[1]
 
 // Receive Content
-async function getContentByLang () {
+async function getContentByLang() {
     let response = await fetch(`/lang/${lang}.json`)
     let data = await response.json()
+
+    adaptiveLang(lang, title, line, titleContainer, contentContainer, subContainer, subType)
 
     restore.textContent = data['Restore']
     title.innerHTML = data['Unlimited Access<br>to All Features']
@@ -78,26 +85,51 @@ annualCard.classList.add('active') // default
 sale.classList.add('active') // default
 
 monthCard.addEventListener('click', (ev) => {
-    if (annualCard.classList.contains('active')){
+    if (annualCard.classList.contains('active')) {
         annualCard.classList.remove('active')
         sale.classList.remove('active')
     }
-    ev.currentTarget.classList.toggle('active')
+    ev.currentTarget.classList.add('active')
 })
 
 annualCard.addEventListener('click', (ev) => {
-    if (monthCard.classList.contains('active')){
+    if (monthCard.classList.contains('active')) {
         monthCard.classList.remove('active')
     }
-    ev.currentTarget.classList.toggle('active')
-    sale.classList.toggle('active')
+    ev.currentTarget.classList.add('active')
+    sale.classList.add('active')
 })
 
 
 button.addEventListener('click', () => {
-    if (monthCard.classList.contains('active')){
+    if (monthCard.classList.contains('active')) {
         document.location = 'https://www.apple.com/'
-    }else {
+    } else {
         document.location = 'https://google.com/'
     }
 })
+
+
+function adaptiveLang(lang, title, line, tc, cc, sc, st) {
+    switch (lang) {
+        case 'es':
+            tc.classList.add('es')
+            break
+        case 'fr':
+            title.classList.add('fr')
+            cc.classList.add('fr')
+            sc.classList.add('fr')
+            line.forEach(item => item.classList.add('fr'))
+            st.forEach(item => item.classList.add('fr'))
+            break
+        case 'ja':
+            title.classList.add('ja')
+            sc.classList.add('ja')
+            break
+        case 'ru':
+            title.classList.add('ru')
+            line.forEach(item => item.classList.add('ru'))
+            break
+        default: return null
+    }
+}
